278db6e8eb799c83ac3d12a6047ec15c8addf901
CottonCandy2004 <805909642@qq.com>
2024-04-14 18:51:29 +0800

    货物相关文件读写实现

--- freightlib.h
+++ freightlib.h
@@ -1,5 +1,55 @@
 #ifndef __FREIGHTLIB_H__
 #define __FREIGHTLIB_H__
 #include "struct.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "ui_terminal.h"
 
+int load_stock_data(freight *stock_data, int *length)
+{
+    FILE *fp;
+    fp = fopen("stock.dat", "r");
+    if (fp == NULL)
+    {
+        fp = fopen("stock.dat", "w");
+        if (fp == NULL)
+        {
+            system("cls");
+            SetColor(4, 0);
+            printf("磁盘读写错误，请检查程序是否有相应目录的读写权限！\n");
+            SetColor(15, 0);
+            system("pause");
+            return -1;
+        }
+    }
+    while (1)
+    {
+        freight *stock_data_temp = (freight *)malloc(sizeof(freight));
+        if (fread(stock_data_temp, sizeof(freight), 1, fp) == 1)
+        {
+            (*length)++;
+            *stock_data = *stock_data_temp;
+            stock_data++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    fclose(fp);
+    return 0;
+}
+
+int save_stock_data(freight *stock_data_head, int *length)
+{
+    FILE *fp2;
+    fp2 = fopen("stock.dat", "w");
+    fwrite(stock_data_head, sizeof(freight), *length, fp2);
+    fclose(fp2);
+    system("cls");
+    SetColor(10, 0);
+    printf("保存成功！\n");
+    SetColor(15, 0);
+    system("pause");
+}
 #endif
