4b8d214d0f6e119046756e918dcd25bf41dc31b6
CottonCandy2004 <805909642@qq.com>
2024-04-16 19:03:34 +0800

    合并模块到主程序

---
+++ stock.h
@@ -0,0 +1,42 @@
+#ifndef STOCK_H
+#define STOCK_H
+#include "struct.h"
+#include <stdio.h>
+#include "freightlib.h"
+#include "ui.h"
+#include "show_stock.h"
+
+void stock_main()
+{
+    freight *stock_data = (freight *)malloc(sizeof(freight) * 3000);
+    freight *stock_data_head = stock_data;
+    int length = 0, changed = 0;
+    int load_satus = load_stock_data(stock_data, &length);
+    if (load_satus == -1)
+        return;
+    stock_data += length;
+    char *notice1 = "请选择您的操作：";
+    char *stock_choice[6] = {"1.进货", "2.收银台", "3.销毁", "4.移除物品", "5.显示库存","6.返回"};
+    char *Error1 = "尚未实现";
+    char *result = "";
+    int ch = ui_choice(notice1, stock_choice, 6);
+    while (1)
+    {
+        if (ch == 4)
+        {
+            show_mode(stock_data_head, length);
+            ch = ui_choice(notice1, stock_choice, 6);
+        }
+        else if (ch == 0 || ch == 1 || ch == 2 || ch == 3)
+        {
+            result = Error1;
+            ch = ui_re_choice(notice1, stock_choice, 6, result, 4);
+        }
+        else if (ch == 5)
+        {
+            return;
+        }
+    }
+}
+
+#endif
