3cd5c363ecaa09917c811dd58f32bc9161da1682
handsomeqwe <15952805508@163.com>
2024-05-07 18:58:11 +0800

    fixes #15

--- destroy.c
+++ destroy.c
@@ -8,11 +8,11 @@
 #include "destroytool.h"
 int main()
 {
-    char* destroy_notice1="请您最后确认是否销毁此商品。";
-    char* choice1="[Y]是";
-    char* choice2="[N]否";
-    char* destroy_choice[2]={choice1, choice2};
-    int load_satus, length = 0, n, i, serch_id,ch1,x;
+    char *destroy_notice1 = "请您最后确认是否销毁此商品。";
+    char *choice1 = "[Y]是";
+    char *choice2 = "[N]否";
+    char *destroy_choice[2] = {choice1, choice2};
+    int load_satus, length = 0, n, i, serch_id, ch1, x;
     unsigned long long EAN;
     freight *p;
     p = (freight *)malloc(sizeof(freight) * 1000);
@@ -46,7 +46,8 @@ int main()
         printf("请输入想要销毁商品的EAN：");
         ShowConsoleCursor();
         scanf("%lld", &EAN);
-        while (EAN < 1e12 || EAN >= 1e13)
+        buff = getchar();
+        while (buff != '\n' || EAN < 1e12 || EAN >= 1e13)
         {
             printf("输入不合法，请重新输入\n");
             fflush(stdin);
@@ -75,17 +76,18 @@ int main()
             printf("您要销毁的商品信息如下：\n");
             printf("EAN码:%lld\n名称:%s\n进货数量:%d\n进货价:%d\n售价:%d\n", p[serch_id].EAN, p[serch_id].name, p[serch_id].stock, p[serch_id].purchase_price, p[serch_id].sale_price);
             system("pause");
-            ch1=ui_choice(destroy_notice1, destroy_choice, 2);
-            if (ch1==0)
+            ch1 = ui_choice(destroy_notice1, destroy_choice, 2);
+            if (ch1 == 0)
             {
-                del_freight(p,&length,serch_id);
+                del_freight(p, &length, serch_id);
                 system("cls");
                 SetColor(10, 0);
                 printf("销毁成功！\n");
                 SetColor(15, 0);
                 fflush(stdin);
             }
-            else{
+            else
+            {
                 printf("已经取消此操作\n");
                 i--;
                 continue;
